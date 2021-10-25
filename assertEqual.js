f7f95fea41556c30e37e467a7d86375b9539d573
Amir <amir.a.23@hotmail.com>
2021-10-25 22:19:01 +0000

    Updated to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@ const assertEqual = function(actual, expected) {
   const failed = String.fromCodePoint(0x1F6D1);
   const passed = String.fromCodePoint(0x2705);
   (actual === expected)
-    ? console.log(passed + passed + passed + " Assertion Passed: " + actual + " === " + expected)
-    : console.log(failed + failed + failed + " Assertion Failed: " + actual + " !== " + expected);
+    ? console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
