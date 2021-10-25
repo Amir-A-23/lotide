eeb3acea0a1ee9477c637ad3d3b75de33660aa77
Amir <amir.a.23@hotmail.com>
2021-10-25 22:39:19 +0000

    function to get the first element of an array

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  const failed = String.fromCodePoint(0x1F6D1);
+  const passed = String.fromCodePoint(0x2705);
+  (actual === expected)
+    ? console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function(arr) {
+  if (arr === undefined || arr.length == 0) {
+    return undefined;
+  }
+  else {
+    return arr[0];
+  } 
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 6);
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
