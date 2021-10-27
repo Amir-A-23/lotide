e4a01f8d7144ed6a706d0fb7c916e9979d14dda1
Amir <amir.a.23@hotmail.com>
2021-10-27 01:34:17 +0000

    created a function to check if the values of arrays are the same

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  const failed = String.fromCodePoint(0x1F6D1);
+  const passed = String.fromCodePoint(0x2705);
+  (actual === expected)
+    ? console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+let eqArrays = (arrayOne, arrayTwo) => {
+  if(arrayOne.length !== arrayTwo.length){
+    return false;
+  } else{
+    for (let i = 0; i < arrayOne.length; i++) {
+      console.log('Array One:', arrayOne[i], 'Array Two:', arrayTwo[i]);
+      if(arrayOne[i] === arrayTwo[i]){
+        //console.log('Array One:', arrayOne[i], 'Array Two:', arrayTwo[i]);
+        match = true;
+      }
+      else{
+        match = false;
+        return match;
+      }
+    }
+    return match
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 3]), true); // => should PASS
