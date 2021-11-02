7ed9982f7d2bb2a9394a151c60ecd0fd96eb4eaf
Amir <amir.a.23@hotmail.com>
2021-11-02 03:08:21 +0000

    refactored tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-
-const num = [1];
-tail(num);
-assertEqual(num.length, 3);
-
-
-const empt = [];
-tail(empt);
-assertEqual(empt.length, 3);
+describe("#tail", () => {
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello, 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns [0] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
