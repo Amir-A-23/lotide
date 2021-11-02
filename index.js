6eb18d267c518e289c50c1c435f9d8f141b4bfc1
Amir <amir.a.23@hotmail.com>
2021-11-02 06:24:21 +0000

    implemented Object Shorthand.

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArray');
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual
 };
